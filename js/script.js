c9d6c500939006992cf393f18dfafdae1d1337b6
FraRussotto <russottofrancesco1@gmail.com>
2023-09-13 19:35:47 +0200

    [ADD] Point 4 Js da fixare

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 1. Creare l'array contente le immagini
 2. Creo le variabili const querySelector
 3. Inserisco le immagini dall'array al miniWrapper con un ciclo
-4. Inserisco le funzioni del btnDown
+4. Inserisco le funzioni del btnDown e del btnUp
 */
 
 
@@ -20,14 +20,15 @@ console.log(archive)
 //2.
 const imageWrapper = document.querySelector('.image_wrapper');
 const miniWrapper = document.querySelector('.mini_wrapper');
-const btnTop = document.querySelector('.up');
+const btnUp = document.querySelector('.up');
 const btnDown = document.querySelector('.down');
 let counterImg = 0;
+console.log(counterImg)
 
 //3.
 for(let i = 0; i < archive.length; i++){
   const image = archive[i];
-  imageWrapper.innerHTML += `<div class="full d-none">
+  imageWrapper.innerHTML += `<div class="full hide">
   <img src="${image}"></div>`
   miniWrapper.innerHTML += `<div class="thumbnail">
   <img src="${image}"></div>`
@@ -36,6 +37,27 @@ for(let i = 0; i < archive.length; i++){
 const fullCollection = document.getElementsByClassName('full');
 const thumbnailCollection = document.getElementsByClassName('thumbnail')
 
-fullCollection[counterImg].classList.remove('d-none')
+fullCollection[counterImg].classList.remove('hide')
 
 
+btnUp.addEventListener('click', function(){
+  //a.
+  fullCollection[counterImg].classList.add('hide')
+  // thumbnailCollection[counterImg].classList.remove('active');
+  //b.
+  counterImg++;
+  console.log(counterImg);
+  //c.
+  fullCollection[counterImg].classList.remove('hide')
+  // thumbnailCollection[counterImg].classList.add('active');
+  });
+
+//5.
+btnDown.addEventListener('click', function(){
+  itemsCollection[counterImg].classList.add('hide')
+//   thumbnailCollection[counterImg].classList.remove('active');
+  counterImg--;
+  itemsCollection[counterImg].classList.remove('hide')
+  // thumbnailCollection[counterImg].classList.add('active');
+  
+});
