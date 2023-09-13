2db958097b640b29609dd660644b6ce230dc493b
FraRussotto <russottofrancesco1@gmail.com>
2023-09-13 16:55:44 +0200

    [ADD] Point 2 JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
 /*
 1. Creare l'array contente le immagini
-
+2. Creo le variabili const querySelector
 */
 
 
 //1.
-const imageWrapper = [
+const image = [
   'img/01.webp',
   'img/02.webp',
   'img/03.webp',
   'img/04.webp',
   'img/05.webp'
 ];
+
+console.log(image)
+
+//2.
+const imageWrapper = document.querySelector('.image_wrapper');
+const miniWrapper = document.querySelector('.mini_wrapper');
+const btnTop = document.querySelector('.up');
+const btnDown = document.querySelector('.down');
+
 
